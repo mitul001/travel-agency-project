01c421b0252df64277063deff148ee38900871d0
miup-odoo <miup@odoo.com>
2024-02-09 16:27:00 +0530

    Set up basic file structure and include description in the manifest file.

---
+++ travelagency/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'travel agency',
+    'version': '1.0',
+    'summary': 'Manage booking, packages, destinations, and accommodations for a travel agency',
+    'description': 'This module provides comprehensive management functionality for a travel agency.',
+    'author': 'Mitul Patel (miup)',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+}
+
