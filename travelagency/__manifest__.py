65c8e5bb6bd4e1cb2e9c3517d116fce4a17279be
miup-odoo <miup@odoo.com>
2024-03-01 16:18:00 +0530

    added demo data

--- travelagency/__manifest__.py
+++ travelagency/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Manage booking, packages, destinations, and accommodations for a travel agency',
     'description': 'This module provides comprehensive management functionality for a travel agency.',
     'author': 'Mitul Patel (miup)',
-    'depends': ['base'],
+    'depends': ['base','mail'],
     'data':[
         'security/ir.model.access.csv',
 
@@ -14,6 +14,13 @@
         'views/travelagency_package_type_view.xml',
         'views/travelagency_customer_view.xml',
         'views/travelagency_menus.xml'
+    ],
+    'demo':[
+        'demo/demo_package_type.xml',
+        'demo/demo_accommodation_type.xml',
+        'demo/demo_package.xml',
+        'demo/demo_accommodation.xml',
+        'demo/demo_customer.xml',
     ],
     'installable': True,
     'application': True,
--- travelagency/models/travelagency_customer.py
+++ travelagency/models/travelagency_customer.py
@@ -1,10 +1,11 @@
-from odoo import fields,models
+from odoo import api,fields,models
 
 class TravelagencyCustomer(models.Model):
     _name = 'travelagency.customer'
     _description = 'Customer of Travel Agency'
+    _inherit = ['mail.thread.main.attachment', 'mail.activity.mixin']
 
-    name = fields.Char(string='Name',required=True)
+    name = fields.Char(string='Name',required=True,tracking=True)
     gender = fields.Selection(string='Gender',selection=[('male','Male'), ('female','Female'), ('other','Other')])
     phone = fields.Char(string='Phone Number',required=True)
     email = fields.Char(string='Email')
@@ -13,9 +14,16 @@ class TravelagencyCustomer(models.Model):
     city = fields.Char(string='City')
     state = fields.Char(string='State')
     country = fields.Char(string='Country')
-    budget = fields.Integer(string='Budget',required=True)
+    package_budget = fields.Float(string='Package Budget',required=True)
+    accommodation_budget = fields.Float(string='Accommodation Budget',required=True)
+    budget = fields.Float(string='Budget',compute="_compute_total_budget")
 
     accommodation_type_id = fields.Many2one('travelagency.accommodation.type')
     packages_type_id = fields.Many2one('travelagency.package.type')
-    package_ids = fields.Many2many('travelagency.packages',required=True)
-    accommodation_ids = fields.Many2many('travelagency.accommodation',required=True)
+    package_ids = fields.Many2many('travelagency.packages',required=True,tracking=True)
+    accommodation_ids = fields.Many2many('travelagency.accommodation',required=True,tracking=True)
+
+    @api.depends('package_budget','accommodation_budget')
+    def _compute_total_budget(self):
+        for record in self:
+            record.budget=record.accommodation_budget+record.package_budget
